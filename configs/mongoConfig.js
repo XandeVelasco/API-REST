82f8695f3c535c5029e671723a4696c64f61b47a
Alexandre Velasco Alves <54082209+XandeVelasco@users.noreply.github.com>
2019-10-12 15:15:19 -0300

    keygen
    test with keygen direct

--- configs/mongoConfig.js
+++ configs/mongoConfig.js
@@ -1,6 +1,7 @@
 let env = process.env.ENV || 'development';
 let mongoConfig = {
-    'development': process.env.ACCOUNT,
+    //'development': process.env.ACCOUNT,
+    'development' : "mongodb+srv://heroku:74Iq3SSGNUuzD4xG@xandeletro-4to4o.mongodb.net/test?retryWrites=true&w=majority",
     'homolog': 'mongodb://localhost:27017/hml',
     'production': 'mongodb://localhost:27017/prd'
 };
